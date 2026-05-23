a7a0f8e9d3593439314c8baf5da1e40fa930030e
weydonan <ahirsch867@gmail.com>
2026-05-23 07:25:24 +0200

    Add files via upload

---
+++ cache_v2.py
@@ -0,0 +1,23 @@
+cache_dict = {}
+
+
+def cache(func):
+    def wrapper(*args):
+        if args in cache_dict:
+            return cache_dict[args]
+        result = func(*args)
+        cache_dict[args] = result
+        return result
+
+    return wrapper
+
+
+@cache
+def fact(n):
+    if n == 0: return 1
+    return n * fact(n - 1)
+
+
+for i in range(0, 10000):
+    fact(i)
+print(fact(1000))
